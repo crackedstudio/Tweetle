88c7cc7b44804bd1b75ba68ddd0c5287383b98f3
manoahLinks <manoahluka@gmail.com>
2024-10-24 15:17:45 +0100

    feat: integrate TMA

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoadingFullPage from "../components/pages/LoadingFullPage";
 import Dashboard from "../components/pages/Dashboard";
 
 const Home = () => {
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setIsLoading(false);
+        }, 5000);
+    
+        // Cleanup the timeout if the component unmounts before the timeout is done
+        return () => clearTimeout(timer);
+      }, []);
+
     return (
         <>
             {isLoading && <LoadingFullPage />}
