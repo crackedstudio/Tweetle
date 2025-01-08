8e09fb68a529e33da0dbdd7dffe3a930f63c09f1
manoahLinks <manoahluka@gmail.com>
2025-01-08 09:43:14 +0100

    feat: outsideExecution

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -4,7 +4,7 @@ import BottomNav from "../components/BottomNav";
 import { ArgentTMA, SessionAccountInterface } from "@argent/tma-wallet";
 import { useEffect, useState } from "react";
 import LoadingFullPage from "../components/pages/LoadingFullPage";
-import { Contract } from "starknet";
+import { CallData, Contract } from "starknet";
 import gameAbi from "../utils/gameAbi.json";
 import useGameLogic from "../hooks/useGameLogic";
 
@@ -103,6 +103,42 @@ const MainLayout = () => {
         setAccount(undefined);
     };
 
+    const handleRegisterPlayer = async () => {
+        
+        console.log(account?.getDeploymentPayload())
+    }
+
+    const handleOutsideExecution = async () => {
+        let calls = [{
+            contractAddress:
+                "0x014348d668e199e0222d2a58d80c04821b9dddb00c5946d1282d415a448227c9",
+            entrypoint: "register_player",
+            // calldata: CallData.compile({
+            //     caller: '0x051fea4450da9d6aee758bdeba88b2f665bcbf549d2c61421aa724e9ac0ced8f',
+            //     source: { type: 0, address: account?.address },
+            // }),
+        }]
+
+        // console.log(account?.getOutsideExecutionPayload({calls}))
+        
+        console.log('sent')
+
+        let call = account?.getOutsideExecutionPayload({calls})
+
+        console.log('call')
+
+        const response = await fetch('http://localhost:6000/player/execute-outside', {
+            method: 'POST',
+            body: JSON.stringify(call)
+        })
+
+        console.log('fetch')
+
+        let result = await response.json()
+
+        console.log(result);
+    }
+
     if (isLoading) {
         return <LoadingFullPage />;
     }
@@ -112,6 +148,10 @@ const MainLayout = () => {
     return (
         <div className="flex flex-col h-[100vh] overflow-hidden text-white relative">
             <main className="flex-grow h-full overflow-auto">
+                <div className="flex flex-col">
+                    <button onClick={handleRegisterPlayer}>Register</button>
+                    <button onClick={handleOutsideExecution}>execute_call</button>
+                </div>          
                 <Outlet
                     context={{
                         account,
