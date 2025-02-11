33ff12dd7fc54e64c0562b9c2e920eb5bb9a997b
Otaiki1 <otaikisadiq2804@gmail.com>
2025-02-11 18:42:05 +0100

    fix: build issues

--- src/components/gameplay/GameTopNav.tsx
+++ src/components/gameplay/GameTopNav.tsx
@@ -1,32 +1,31 @@
 import CoinPill from "../ui/CoinPill";
-import right from "../../assets/right.png";
 import { useOutletContext, useNavigate } from "react-router-dom";
 import { ChevronLeft } from "lucide-react";
 
 interface GameData {
-  points: number;
-  // add other properties that might exist
+    points: number;
+    // add other properties that might exist
 }
 
 interface OutletContextType {
-  playerDetails: GameData;
+    playerDetails: GameData;
 }
 
 const GameTopNav = () => {
-  const { playerDetails } = useOutletContext<OutletContextType>();
-  const navigate = useNavigate();
+    const { playerDetails } = useOutletContext<OutletContextType>();
+    const navigate = useNavigate();
 
-  return (
-    <div className="flex justify-between px-3 items-center py-3">
-      <button
-        className="flex items-center bg-[#C4C4C429] rounded-full text-white gap-x-1 py-2 px-3 text-xs"
-        onClick={() => navigate(-1)}
-      >
-        <ChevronLeft size={13} /> Back
-      </button>
-      <CoinPill text={String(playerDetails?.points)} />
-    </div>
-  );
+    return (
+        <div className="flex justify-between px-3 items-center py-3">
+            <button
+                className="flex items-center bg-[#C4C4C429] rounded-full text-white gap-x-1 py-2 px-3 text-xs"
+                onClick={() => navigate(-1)}
+            >
+                <ChevronLeft size={13} /> Back
+            </button>
+            <CoinPill text={String(playerDetails?.points)} />
+        </div>
+    );
 };
 
 export default GameTopNav;
